094c44f3bae6e48aa333b868afe418ba4924700a
Rishi Bhardwaj <rishibhardwaj1210@gmail.com>
2026-07-26 00:55:01 +0530

    Fix #318: Improve performance of the Kepler orbit solver

--- src/lib/kepler.ts
+++ src/lib/kepler.ts
@@ -151,3 +151,6 @@ export function hohmannDeltaVKmPerSec(r1Km: number, r2Km: number): number {
   const dV2 = Math.abs(v2 - vApogee)
   return dV1 + dV2
 }
+
+// Cached PI constants for Kepler solver
+export const PI2 = Math.PI * 2;
